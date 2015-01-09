29257f3aac640550933ddab03510b77453f0e0a9
Blair Wright <blairbwright@gmail.com>
2015-01-09 15:02:29 -0800

    new

--- public_html/script.js
+++ public_html/script.js
@@ -7,5 +7,19 @@
 $("document").ready(function() {
     $(".wwb").css("background-color", "red");
     $("p:last").css("background-color", "pink");
+    $('h1').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $('h1').bind('click', mouseClick);
+    
 });
+
+function mouseOverMe() {
+    $("h1").html("This is easy");
+}
+function MouseOutMe() {
+    $('hi').html('I am Cool');
+}
+function MouseClick() {
+    $('p').html('swagswagswag');
+}
 
